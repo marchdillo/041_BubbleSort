47a5a70df87db678ab9d1817ceb0a84234d1d6bc
marchdillo <kevinno.limiy@gmail.com>
2025-03-13 12:34:37 +0700

    prosedur bubblesortarray

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int arr[20];    //deklarasi variable global array a dengan ukuran 20
 int n;          //deklarasi variable global n untuk menyimpan banyaknya elemen pada array
 
-void input() //prosedur untuk input
+void input()
 {
     while (true) {
         cout << "Masukkan banyaknya elemen pada array:";
@@ -28,4 +28,25 @@ void input() //prosedur untuk input
         cout << "Data ke-" << (i+1) << "i";
         cin >> arr[i];
     }
+}
+
+void bubbleSortArray(){
+    int pass = 1; //step 1
+
+    do
+    {//step 2
+        for (int j = 0; j <= n -1-pass;j++) //step 3
+    {
+        if (arr[j] > arr[j+1]){
+            int temp;
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+
+        pass = pass + 1; //step 4
+    }
+        
+
+    }while (pass <= n - 1); //step 5
 }
